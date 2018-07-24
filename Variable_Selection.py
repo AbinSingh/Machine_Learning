feb2f876e7a665f0bc362e3d5516ee594cdbe5c9
AbinSingh <abin.astro@gmail.com>
2018-07-25 02:50:18 +0530

    Variable selection module for statsmodels
    This module contains the backward elimination function which is used to choose the most significant variable and returns the final model.

---
+++ Variable_Selection.py
@@ -0,0 +1,23 @@
+                         # Backward_Elimination #
+# Backward elimination is one of the variable selection method used to choose # the most significant variable
+                     
+                       # Function inputs or arguments
+
+# First parameter - result of full model # Example result = model.fit() 
+# Second paramter - threshold # Alpha value like 0.1,0.05 etc
+# train_x # DataFrame with only predictor variables
+# train_y # DataFrame with only independent variables
+    
+import numpy as np
+import pandas as pd
+import statsmodels.formula.api as sm
+
+rem=[] 
+def Backward_Elimination(result,threshold,train_y,train_x):
+    while np.amax(result.pvalues) > threshold:
+      value_name=pd.Series(list(result.pvalues.values),index=result.pvalues.index)
+      rem.append(value_name.idxmax())
+      train_xx=train_x.drop(rem,axis=1)
+      model=sm.Logit(train_y,train_xx)
+      result=model.fit()
+    return result
